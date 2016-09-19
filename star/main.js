2197cd82f31ecf97cfd5e3ab8da4ec69df1c4e3f
pitty079 <pit6sic6@gmail.com>
2016-09-19 16:06:03 +0300

    add +

--- star/main.js
+++ star/main.js
@@ -1,11 +1,11 @@
 var num = 1;
 for ( ; num < 101; num++ ) {
     if ( num % 5 === 0 && num % 3 === 0 ){
-        console.log( num, "FizzBuzz" )
+        console.log( num + " FizzBuzz" )
     }else if ( num%5 === 0 ){
-        console.log( num, "Buzz" );
+        console.log( num + " Buzz" );
     }else if ( num%3 === 0 ){
-        console.log( num, "Fizz" );
+        console.log( num + " Fizz" );
     }else{
         console.log( num );
     }
