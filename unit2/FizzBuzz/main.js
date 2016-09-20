0b991818a25ba182c5c7f5dc44dccca445687f25
pitty079 <pit6sic6@gmail.com>
2016-09-20 08:48:44 +0300

    after reformat code

--- unit2/FizzBuzz/main.js
+++ unit2/FizzBuzz/main.js
@@ -1,12 +1,12 @@
 var num = 1;
-for ( ; num < 101; num++ ) {
-    if ( num % 5 === 0 && num % 3 === 0 ){
-        console.log( num + " FizzBuzz" )
-    }else if ( num%5 === 0 ){
-        console.log( num + " Buzz" );
-    }else if ( num%3 === 0 ){
-        console.log( num + " Fizz" );
-    }else{
-        console.log( num );
+for (num; num < 101; num++) {
+    if (num % 5 === 0 && num % 3 === 0) {
+        console.log(num + " FizzBuzz")
+    } else if (num % 5 === 0) {
+        console.log(num + " Buzz");
+    } else if (num % 3 === 0) {
+        console.log(num + " Fizz");
+    } else {
+        console.log(num);
     }
 }
