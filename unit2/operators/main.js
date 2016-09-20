c34c490e8bcb469457d79577764de29c222f0118
pitty079 <pit6sic6@gmail.com>
2016-09-20 08:52:47 +0300

    after proper formatting

--- unit2/operators/main.js
+++ unit2/operators/main.js
@@ -4,11 +4,15 @@ var a = 1, b = 1, c, d;
 
 var a = 1, b = 1, c, d;
 
-c =++a; alert(c);// 2 prefix return new value (1+1)
-d = b++; alert(d);// 1 postfix return old value (1+1-1)
+c = ++a;
+alert(c);// 2 prefix return new value (1+1)
+d = b++;
+alert(d);// 1 postfix return old value (1+1-1)
 
-c = 2 + ++a; alert(c);// 5 new value of a equals 3 (2+1)
-d = 2 + b++; alert(d);// 4 previous value of b equals 2 (2+1-1)
+c = 2 + ++a;
+alert(c);// 5 new value of a equals 3 (2+1)
+d = 2 + b++;
+alert(d);// 4 previous value of b equals 2 (2+1-1)
 
 alert(a);// 3 last value of a equals 3 (1+1+1)
 alert(b);// 3 last value of b equals 3 (1+1+1)
