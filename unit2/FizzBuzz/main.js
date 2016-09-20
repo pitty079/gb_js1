8bc49c7f0632d237851596ef5231adbd8740148f
pitty079 <pit6sic6@gmail.com>
2016-09-20 09:16:55 +0300

    minimizing code

--- unit2/FizzBuzz/main.js
+++ unit2/FizzBuzz/main.js
@@ -1,5 +1,4 @@
-var num = 1;
-for (num; num < 101; num++) {
+for (var num = 1; num < 101; num++) {
     if (num % 5 === 0 && num % 3 === 0) {
         console.log(num + " FizzBuzz")
     } else if (num % 5 === 0) {
